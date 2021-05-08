77a23d616cad91157287640e2943b7ab2a2c60c2
n2ptune <n2ptune@outlook.com>
2021-05-08 18:09:49 +0900

    fix: typo firebase envrionment key

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -7,7 +7,7 @@ initializeApp({
   authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
   projectId: process.env.VUE_APP_FB_PROJECT_ID,
   storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_FB_APP_ID,
   measurementId: process.env.VUE_APP_FB_MEASUREMENT_ID
 })
